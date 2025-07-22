7a5eba64c12dc0af08599700a4c35b2a85f698e5
Karen <karen.perez.0395@gmail.com>
2025-07-22 17:47:36 -0300

    inicio de maquetacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,57 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Abyssinica_SIL } from 'next/font/google';
 import styles from "./page.module.css";
+
+const abyssinica = Abyssinica_SIL({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap',
+});
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.container_ppl}>
+      <div className={styles.home}>
+        <div className={`${abyssinica.className} ${styles.container_title}`}>
+          <h1 className={styles.title}>¡Descubri los diferentes servicios que tenemos para ofrecerte!</h1>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.container_img}>
+          <Link href={"https://transporteecologico.com.ar"}>
+            <Image src="/image/transporte.png" alt="Transporte" width={600} height={180} />
+          </Link>
+          <Link href={"https://transporteecologico.com.ar"}>
+            <Image src="/image/equipos.png" alt="Equipos" width={600} height={180} />
+          </Link>
+          <Link href={"https://transporteecologico.com.ar"}>
+            <Image src="/image/estaciones.png" alt="Estaciones" width={600} height={180} />
+          </Link>
+        </div>
+      </div>
+      <div className={styles.container_p}>
+        <div className={styles.mision}>
+          <p className={styles.subtitle}>Misión</p>
+          <p>Ofrecer transporte sostenible con camiones euro 6, a gas nacional reduciendo el uso de combustible diesel, importado mejorando la contaminación ambiental y sonora.
+            Desarrollar la transformación a gas de vehículos ciclos otto y diesel,
+            Desarrollar los corredores verdes para producir un efecto económico positivo en la economía.</p>
+        </div>
+        <div className={styles.vision}>
+          <p className={styles.subtitle}>Vision</p>
+          <p>Ser socios confiables y seguros de aquellas empresas comprometidas con el medio ambiente y la reducción de la huella de carbono ofreciendo servicios de transporte ecológico, en el ámbito nacional e internacional.</p>
+        </div>
+        <div className={styles.valores}>
+          <p className={styles.subtitle}>Valores</p>
+          <p>Profesionalización de nuestros servicios
+            Uso de normas internacionales de calidad.<br/>
+            Mejora continua<br/>
+            Compromiso ambiental</p>
+        </div>
+      </div>
+      <div className={styles.container_descripcion}>
+        <div>
+          <h2>Descripción</h2>
+        </div>
+      </div>
     </div>
   );
 }
