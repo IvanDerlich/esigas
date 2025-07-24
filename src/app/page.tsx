f4887bd7fcdb40cbbe0d06764eb325d1d14ac9e4
Ivan Derlich <a@ivanderlich.com>
2025-07-24 00:34:14 -0300

    Extract long strings into another file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,17 +78,7 @@ export default function Home() {
         </div>
         <div className={styles.container_descripcion}>
           <p className={`${abyssinica.className} ${styles.descripcion}`}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. Lorem Ipsum is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. Lorem Ipsum is simply dummy text of the printing
-            and typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown printer
-            took a galley of type and scrambled it to make a type specimen book.
+            {strings.descripcion}
           </p>
           <Image
             className={styles.img_descripcion}
