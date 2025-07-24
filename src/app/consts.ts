b9b3701afcc526a077993680f89fc06bc53be6d9
Ivan Derlich <a@ivanderlich.com>
2025-07-24 00:28:52 -0300

    Extract text to a const file

---
+++ src/app/consts.ts
@@ -0,0 +1,8 @@
+export const strings = {
+  mission:
+    "Ofrecer transporte sostenible con camiones euro 6, a gas nacional reduciendo el uso de combustible diesel, importado mejorando la contaminación ambiental y sonora. Desarrollar la transformación a gas de vehículos ciclos otto y diesel, Desarrollar los corredores verdes para producir un efecto económico positivo en la economía.",
+  vision:
+    "Ser socios confiables y seguros de aquellas empresas comprometidas con el medio ambiente y la reducción de la huella de carbono ofreciendo servicios de transporte ecológico, en el ámbito nacional e internacional.",
+  valores:
+    "Profesionalización de nuestros servicios. Uso de normas internacionales de calidad. Mejora continua. Compromiso ambiental.",
+};
