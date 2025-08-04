39fa0f6739cda83eb59d8f10bd4ac4c6951a71bc
Karen <karen.perez.0395@gmail.com>
2025-08-04 17:08:28 -0300

    Format

--- src/app/estaciones/page.tsx
+++ src/app/estaciones/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
-import Estacion1 from "@/images/estacion1.png"
-import Estacion2 from "@/images/estacion2.png"
-import Estacion3 from "@/images/estacion3.png"
-import Estacion4 from "@/images/estacion4.png"
-import Estacion5 from "@/images/estacion5.png"
-import Estacion6 from "@/images/estacion6.png"
-import Line from "@/images/line.png"
+import Estacion1 from '@/images/estacion1.png';
+import Estacion2 from '@/images/estacion2.png';
+import Estacion3 from '@/images/estacion3.png';
+import Estacion4 from '@/images/estacion4.png';
+import Estacion5 from '@/images/estacion5.png';
+import Estacion6 from '@/images/estacion6.png';
+import Line from '@/images/line.png';
 import logo from '@/images/logo.png';
-import Camion from "@/images/camion.png"
+import Camion from '@/images/camion.png';
 import Link from 'next/link';
 import styles from './page.module.css';
 import { Abyssinica_SIL } from 'next/font/google';
@@ -66,16 +66,22 @@ export default function Page() {
         <OurAdvantages />
         <div>
           <div className={`${abyssinica.className} ${styles.estaciones}`}>
-            <h3 className={styles.estacionesTitle}>Nuestras Estaciones de Servicio</h3>
+            <h3 className={styles.estacionesTitle}>
+              Nuestras Estaciones de Servicio
+            </h3>
           </div>
           <div className={`${abyssinica.className} ${styles.containerMaps}`}>
             <div className={styles.maps}>
               <div className={styles.mapContent}>
-                <h4 className={styles.mapTitle}>Saavedra 101, Guaymallén, Mendoza</h4>
-                <p className={styles.mapItems}>6 ESTACIONES DE CARGA <br />
+                <h4 className={styles.mapTitle}>
+                  Saavedra 101, Guaymallén, Mendoza
+                </h4>
+                <p className={styles.mapItems}>
+                  6 ESTACIONES DE CARGA <br />
                   SHOP CAFE COMIDAS PRODUCTOS BASICOS <br />
                   LEÑA HIELO <br />
-                  ACEITE AGUA DESTILADA</p>
+                  ACEITE AGUA DESTILADA
+                </p>
               </div>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.244238680631!2d-68.83192682453506!3d-32.89171017361593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e093c98fcf3bf%3A0x8699a83fb67a3a27!2sSaavedra%20101%2C%20M5519%20Mendoza!5e0!3m2!1sen!2sar!4v1754329864541!5m2!1sen!2sar"
@@ -123,11 +129,15 @@ export default function Page() {
                 className={styles.ubication}
               />
               <div className={styles.mapContent}>
-                <h4 className={styles.mapTitle}>Sta Fe 5482 Oeste, Rosario, Santa Fe</h4>
-                <p className={styles.mapItems}>6 ESTACIONES DE CARGA <br />
+                <h4 className={styles.mapTitle}>
+                  Sta Fe 5482 Oeste, Rosario, Santa Fe
+                </h4>
+                <p className={styles.mapItems}>
+                  6 ESTACIONES DE CARGA <br />
                   SHOP CAFE COMIDAS PRODUCTOS BASICOS <br />
                   LEÑA HIELO <br />
-                  ACEITE AGUA DESTILADA</p>
+                  ACEITE AGUA DESTILADA
+                </p>
               </div>
             </div>
             <div className={styles.mapImage}>
@@ -171,14 +181,25 @@ export default function Page() {
               height={600}
               className={styles.transport}
             />
-            <div className={`${abyssinica.className} ${styles.transporteContainer}`}>
+            <div
+              className={`${abyssinica.className} ${styles.transporteContainer}`}
+            >
               <div className={styles.transporteText}>
-                <h4 className={styles.transporteTitle}>Habilitadas para carga pesada</h4>
-                <p className={styles.transporteSub}>Nuestras estaciones están preparadas para recibir vehiculos de gran porte.</p>
+                <h4 className={styles.transporteTitle}>
+                  Habilitadas para carga pesada
+                </h4>
+                <p className={styles.transporteSub}>
+                  Nuestras estaciones están preparadas para recibir vehiculos de
+                  gran porte.
+                </p>
                 <div className={styles.transporteItems}>
                   <p className={styles.item}>Áreas Amplias de maniobrar</p>
-                  <p className={styles.item}>Surtidores adaptados para camiones y colectivos</p>
-                  <p className={styles.item}>Pisos reforzados para alto tonelaje</p>
+                  <p className={styles.item}>
+                    Surtidores adaptados para camiones y colectivos
+                  </p>
+                  <p className={styles.item}>
+                    Pisos reforzados para alto tonelaje
+                  </p>
                   <p className={styles.item}>Accesos señalizados y seguros</p>
                 </div>
               </div>
