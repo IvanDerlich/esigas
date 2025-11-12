71d7d0fe3f67e652ceba47e2bf25e30af48b99fd
Karen <karen.perez.0395@gmail.com>
2025-11-12 16:45:57 -0300

    Agrego seccion de correo institucional y correo en footer

--- src/app/components/layout/footer.tsx
+++ src/app/components/layout/footer.tsx
@@ -13,10 +13,21 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <Image src={logo} alt="Logo" width={110} height={55} />
-      <div className={styles.footer_text}>
-        <p className={`${abyssinica.className} ${styles.footer_p}`}>
-          Made by Datara in 2025
-        </p>
+      <div className={styles.content_footer}>
+        <div className={styles.footer_text}>
+          <p className={`${abyssinica.className} ${styles.footer_p}`}>
+            Made by Datara in 2025
+          </p>
+        </div>
+        <div className={`${abyssinica.className} ${styles.contact_info}`}>
+          <p className={styles.title_contacto}>Contacto:</p>
+          <a
+            className={styles.email_contacto}
+            href="mailto:esigas@esigas.com.ar?subject=Consulta%20de%20servicio&body=Hola,%20quisiera%20consultar%20sobre..."
+          >
+            esigas@esigas.com.ar
+          </a>
+        </div>
       </div>
     </footer>
   );
