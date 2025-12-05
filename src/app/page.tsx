ddba1fbc00ec3a1d8f332611375bb1eeda2dae4c
Karen <karen.perez.0395@gmail.com>
2025-12-05 15:46:41 -0300

    Cambio de diseño de sección descripción

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,29 @@
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
-import { Abyssinica_SIL } from 'next/font/google';
 import styles from './page.module.css';
-import descripcion_img from '@/images/descripcion_img.jpg';
 import { Footer } from './components/layout/footer';
 import { strings } from './consts';
 import AccordionSlider from './components/accordionSlider/accordionSlider';
-
-const abyssinica = Abyssinica_SIL({
-  subsets: ['latin'],
-  weight: ['400'],
-  display: 'swap',
-});
+import OurCompany from './components/ourCompany/ourCompany';
 
 export default function Home() {
   return (
     <>
       <main className={styles.container_ppl}>
         <AccordionSlider />
+        <OurCompany />
         <div className={styles.container_p}>
           <div className={styles.mision}>
-            <p className={`${abyssinica.className} ${styles.subtitle}`}>
-              Misión
-            </p>
-            <p className={abyssinica.className}>{strings.mission}</p>
+            <p className={styles.subtitle}>Misión</p>
+            <p className={styles.mision}>{strings.mission}</p>
           </div>
           <div className={styles.vision}>
-            <p className={`${abyssinica.className} ${styles.subtitle}`}>
-              Visión
-            </p>
-            <p className={abyssinica.className}>{strings.vision}</p>
+            <p className={styles.subtitle}>Visión</p>
+            <p className={styles.vision}>{strings.vision}</p>
           </div>
           <div className={styles.valores}>
-            <p className={`${abyssinica.className} ${styles.subtitle}`}>
-              Valores
-            </p>
-            <p className={abyssinica.className}>{strings.valores}</p>
-          </div>
-        </div>
-        <div className={styles.container_ppl_descripcion}>
-          <div className={styles.container_h2}>
-            <h2 className={`${abyssinica.className} ${styles.title_h2}`}>
-              Descripción
-            </h2>
-          </div>
-          <div className={styles.container_descripcion}>
-            <p className={`${abyssinica.className} ${styles.descripcion}`}>
-              {strings.descripcion}
-            </p>
-            <Image
-              className={styles.img_descripcion}
-              src={descripcion_img}
-              alt="Descripcion"
-              width={400}
-              height={400}
-            />
+            <p className={styles.subtitle}>Valores</p>
+            <p className={styles.valores}>{strings.valores}</p>
           </div>
         </div>
         <div className={styles.container_contacto}>
