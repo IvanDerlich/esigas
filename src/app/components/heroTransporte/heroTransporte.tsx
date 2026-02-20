3770c49dc4922d6474e1ca059490b28820fc2606
Karen <karen.perez.0395@gmail.com>
2026-02-20 16:16:02 -0300

    Mediaquery para tablet y mobile

--- src/app/components/heroTransporte/heroTransporte.tsx
+++ src/app/components/heroTransporte/heroTransporte.tsx
@@ -4,6 +4,7 @@ import Cloud from '@/images/cloud.png';
 import HeroImage from '@/images/hero-transporte.png';
 import Link from 'next/link';
 import { poppins, bebas } from '@/app/assets/fonts';
+import HeroMobile from '@/images/hero-transporte-mobile.png';
 
 export default function HeroTransporte() {
   return (
@@ -16,17 +17,28 @@ export default function HeroTransporte() {
           alt="Hero Imagen"
           width={526}
           height={598}
+          priority
+        />
+        <Image
+          className={styles.heroImageMobile}
+          src={HeroMobile}
+          alt="Hero Imagen Mobile"
+          width={320}
+          height={400}
+          priority
         />
       </div>
       <div className={`${styles.containerText} ${bebas.className}`}>
-        <p className={styles.title}>TRANSPORTE ECOLÓGICO</p>
-        <div className={styles.textSlider}>
-          <p className={`${styles.line} ${styles.line1}`}>
-            TRANSPORTAMOS A GNC
-          </p>
-          <p className={`${styles.line} ${styles.line2}`}>
-            CUIDAMOS EL PLANETA
-          </p>
+        <div className={styles.textBox}>
+          <p className={styles.title}>TRANSPORTE ECOLÓGICO</p>
+          <div className={styles.textSlider}>
+            <p className={`${styles.line} ${styles.line1}`}>
+              TRANSPORTAMOS A GNC
+            </p>
+            <p className={`${styles.line} ${styles.line2}`}>
+              CUIDAMOS EL PLANETA
+            </p>
+          </div>
         </div>
         <Link
           className={`${styles.link} ${poppins.className}`}
