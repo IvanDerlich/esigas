3f8471213d9c29ed669ae35b0356c21b1cd754bf
Karen <karen.perez.0395@gmail.com>
2026-03-05 14:58:26 -0300

    Cambio de correo de contacto en página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
           <h3 className={styles.title_contacto}>Comunicate con nosotros:</h3>
           <a
             className={styles.email_contacto}
-            href="mailto:esigas@esigas.com.ar?subject=Consulta%20de%20servicio&body=Hola,%20quisiera%20consultar%20sobre..."
+            href="mailto:info@esigas.com.ar?subject=Consulta%20de%20servicio&body=Hola,%20quisiera%20consultar%20sobre..."
           >
-            esigas@esigas.com.ar
+            info@esigas.com.ar
           </a>
         </div>
       </main>
